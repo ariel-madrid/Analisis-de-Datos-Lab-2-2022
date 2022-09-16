0fce873f290c652b11d3a86e5f6a2f49aa067464
ariel.argomedo@usach.cl <73253466+ariel-madrid@users.noreply.github.com>
2022-09-16 14:15:02 -0300

    Se muestra mediante histograma el procentaje de missing data por variable

--- Laboratorio 1/Hepatitis-Data.R
+++ Laboratorio 1/Hepatitis-Data.R
@@ -1,8 +1,8 @@
 #Importar librerias
 library(dplyr)
-library(tidyverse)
 library(ggpubr)
 library(ggplot2)
+library(VIM)
 
 #Leer data set de Hepatitis.
 data <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data", fileEncoding = "UTF-8", sep = ",")
@@ -12,15 +12,31 @@ names(data) <- c('Class', 'Age', 'Sex', 'Steroid', 'Antivirals', 'Fatigue', 'Mal
                  'Spiders', 'Ascites', 'Varices', 'Bilirubin', 'Alk_Phosphate', 'Sgot', 'Albumin', 'Protime', 'Histology')
 
 
-# Obtener columna que posee más valores sin documentar
+# Obtener columna que posee m?s valores sin documentar
 
 missing_values_recount <- data %>% summarise_all(~ sum(. == "?"))
 
-# Obtener qué paciente posee la mayor cantidad de atributos sin documentar
+# Obtener qu? paciente posee la mayor cantidad de atributos sin documentar
 missing_values_per_patient <- rowSums(data == "?")
 max_missing_values_patient <- which(missing_values_per_patient == max(missing_values_per_patient))
 
+#Cambiar '?' valores a NA
+data <- data %>% mutate_all(~na_if(., "?"))
+
+#Visualizar datos faltantes en el data set - Muestra porcentajes de missing data por variable.
+
+histogram_of_missing_values <- suppressWarnings(aggr(data, col=c('navyblue','red'), 
+                               numbers=TRUE, sortVars=TRUE, labels=names(data), cex.axis=.7, 
+                               gap=3, ylab=c("Histograma de valores faltantes en data set","Patron")))
 
 #Estadisticas descriptivas ----
+
+
+
+
+
+
+
+
 
 #Estadisticas inferenciales ----
