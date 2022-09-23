f4c2f50b5afa8f38b3627ec5148d9264a5c3a685
ariel-madrid <ariel.argomedo@usach.cl>
2022-09-22 21:59:32 -0300

    Subi mi codigo ahi mas ordenado en el mismo script

--- Laboratorio 1/Hepatitis-Data.R
+++ Laboratorio 1/Hepatitis-Data.R
@@ -5,6 +5,9 @@ library(ggplot2)
 library(VIM)
 library(tidyr)
 library(naniar)
+library(corrplot)
+library(nortest)
+library(car)
 #Leer data set de Hepatitis.
 data <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data", fileEncoding = "UTF-8", sep = ",")
 
@@ -44,27 +47,28 @@ data$Protime <- NULL
 #Eliminar top 10 filas con mas valores NA's
 data$id <- seq(1:155)
 data <- data[!(data$id %in% ids_patient_top_ten_missing_Values), ]
+data$id <-NULL
 
 #Convertir las columnas a los formatos correctos
-data$Class <- as.character(data$Class)
+data$Class <- as.factor(data$Class)
 data$Age <- as.integer(data$Age)
-data$Sex <- as.character(data$Sex)
-data$Steroid <- as.character(data$Steroid)
-data$Antivirals <- as.character(data$Antivirals)
-data$Fatigue <- as.character(data$Fatigue)
-data$Malaise <- as.character(data$Malaise)
-data$Anorexia <- as.character(data$Anorexia)
-data$Liver_Big <- as.character(data$Liver_Big)
-data$Liver_Firm < as.character(data$Liver_Firm)
-data$Spleen_Palpable <- as.character(data$Spleen_Palpable)
-data$Spiders <- as.character(data$Spiders)
-data$Ascites <- as.character(data$Ascites)
-data$Varices <- as.character(data$Varices)
+data$Sex <- as.factor(data$Sex)
+data$Steroid <- as.factor(data$Steroid)
+data$Antivirals <- as.factor(data$Antivirals)
+data$Fatigue <- as.factor(data$Fatigue)
+data$Malaise <- as.factor(data$Malaise)
+data$Anorexia <- as.factor(data$Anorexia)
+data$Liver_Big <- as.factor(data$Liver_Big)
+data$Liver_Firm <- as.factor(data$Liver_Firm)
+data$Spleen_Palpable <- as.factor(data$Spleen_Palpable)
+data$Spiders <- as.factor(data$Spiders)
+data$Ascites <- as.factor(data$Ascites)
+data$Varices <- as.factor(data$Varices)
 data$Bilirubin <- as.numeric(data$Bilirubin)
 data$Alk_Phosphate <- as.integer(data$Alk_Phosphate)
-data$Sgot < as.integer(data$Sgot)
+data$Sgot <- as.numeric(data$Sgot)
 data$Albumin <- as.numeric(data$Albumin)
-data$Histology <- as.character(data$Histology)
+data$Histology <- as.factor(data$Histology)
 
 #Obtener la moda de una determinada columna
 getmode <- function(v){
@@ -85,14 +89,14 @@ for (cols in colnames(data)) {
 
 # Estadisticas descriptivas ----
 
-# Para las primeras 10 variables, 9 son categóricas.
-# Se hará un recuento de la frecuencia para cada una.
+# Para las primeras 10 variables, 9 son categ?ricas.
+# Se har? un recuento de la frecuencia para cada una.
 
 
-# Función que entrega un gráfico de las variables categóricas dicotómicas que solo
-# entregan información de si un síntoma está presente o no.
+# Funci?n que entrega un gr?fico de las variables categ?ricas dicot?micas que solo
+# entregan informaci?n de si un s?ntoma est? presente o no.
 # Entrada: datos: dataframe/table, column_name: string
-# Salida: gráfico de barras
+# Salida: gr?fico de barras
 summarise_aux <- function(data, column_name){
   data <- data.frame(x = c("Presente", "No presente"), 
                      total = count(data, get(column_name)) %>% pull())
@@ -107,16 +111,82 @@ anti_plt <- summarise_aux(data, "Antivirals")
 fatigue_plt <- summarise_aux(data, "Fatigue")
 cathegorical_plots_pt_1 <- ggarrange(gender_plt, steroid_plt, anti_plt, fatigue_plt,
                                      ncol = 2, nrow = 2)
-
-
 malaise_plt <- summarise_aux(data, "Malaise")
 anorex_plt <- summarise_aux(data, "Anorexia")
-liver_b_plt <- summarise_aux(data, "Liver_Big") + scale_x_discrete("Hígado Agrandado")
-liver_f_plt <- summarise_aux(data, "Liver_Firm") + scale_x_discrete("Hígado Firme")
-cathegorical_plots_pt_2 <- ggarrange(malaise_plt, anorex_plt, liver_b_plt, liver_f_plt,
-                                     ncol = 2, nrow = 2)
+liver_b_plt <- summarise_aux(data, "Liver_Big") + scale_x_discrete("Higado Agrandado")
+liver_f_plt <- summarise_aux(data, "Liver_Firm") + scale_x_discrete("Higado Firme")
+cathegorical_plots_pt_2 <- ggarrange(malaise_plt, anorex_plt, liver_b_plt, liver_f_plt, ncol = 2, nrow = 2)
+
+#Grafico de frecuencias para reconocer la moda de la variable Spleen Palpable
+barplot_spleen_palpable <- ggplot(data,aes(x=factor(Spleen_Palpable)))+ geom_bar(position="dodge")+ labs(title="", x="Spleen Palpable", y = "Frecuencia")+
+  geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
+
+#Grafico de frecuencias para reconocer la moda de la variable Spiders
+barplot_spiders <- ggplot(data,aes(x=factor(Spiders)))+ geom_bar(position="dodge")+ labs(title="", x="Spiders", y = "Frecuencia")+
+  geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
+
+#Grafico de frecuencias para reconocer la moda de la variable Ascites
+barplot_ascites <- ggplot(data,aes(x=factor(Ascites)))+ geom_bar(position="dodge")+ labs(title="", x="Ascites", y = "Frecuencia")+
+  geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
+
+#Grafico de frecuencias para reconocer la moda de la variable Varices
+barplot_varices <- ggplot(data,aes(x=factor(Varices)))+ geom_bar(position="dodge")+ labs(title="", x="Varices", y = "Frecuencia")+
+  geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
+
+#Grafico de frecuencias para reconocer la moda de la variable Histology
+barplot_histology <- ggplot(data,aes(x=factor(Histology)))+ geom_bar(position="dodge")+ labs(title="", x="Histology", y = "Frecuencia")+
+  geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
+
+#Combinar figuras
+final_barplot_combined <- ggarrange(barplot_spleen_palpable,barplot_spiders,barplot_ascites,barplot_varices,barplot_histology, nrow=2,ncol=4)
+print(final_barplot_combined)
+
+#Obtener matriz de correlacion para observar correlacion de las variables numericas de la base de datos.
+correlacion<-round(cor(select_if(data, is.numeric)), 1)
+
+corrplot(correlacion, method="number", type="upper")
+
+#Diagrama de dispersion que muestra leve relacion inversa entre Bilirubin y Albumin
+bilirubin_albumin_cor <- scatterplot(Bilirubin ~ Albumin,data = data, smooth = FALSE, grid = F, frame = F)
+print(bilirubin_albumin_cor)
+
+#Visualizar distribucion de variables numericas continuas
+
+#Bilirubin distribution
+bilirubin_distribution <- ggplot(data, aes(x=Bilirubin)) + geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FFF666") + labs(title="", y = "Densidad") + 
+  geom_vline(aes(xintercept=mean(Bilirubin, na.rm=T)), color="red", linetype="dashed", size=1)
+
+#Albumin distribution
+albumin_distribution <- ggplot(data, aes(x=Albumin)) + geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FFF666") + labs(title="", y = "Densidad") + 
+  geom_vline(aes(xintercept=mean(Albumin, na.rm=T)), color="red", linetype="dashed", size=1)
+
+#Combinar graficos
+final_plot_distribution <- ggarrange(bilirubin_distribution,albumin_distribution, nrow=1,ncol=2)
+print(final_plot_distribution)
+
+#Grafica de barras segmentadas para Class + Spleen_Palpable
+levels(data$Class) <- c("Muerto","Vivo")
+levels(data$Spleen_Palpable) <- c("Yes", "No")
+contingency_table  <- xtabs(~ Class + Spleen_Palpable , data = data)
+contingency_table  <- as.data.frame(contingency_table)
+
+#Se crea el grafico
+segmented_bar_plot <- ggplot(contingency_table, aes(fill = Class , y = Freq , x = Spleen_Palpable))
+segmented_bar_plot <- segmented_bar_plot + geom_bar(position = "stack", stat = "identity")
+segmented_bar_plot <- segmented_bar_plot + labs(y = "Frecuencia") + ggtitle("Barras  apiladas para Class y Spleen Palpable")
+segmented_bar_plot <- segmented_bar_plot + theme_pubr()
+
+print(segmented_bar_plot)
 
 # An?lisis inferencial ----
 
+#Test Lilliefors para determinar si variable Albumin sigue una distribucion normal.
 
+#H0: La distribucion es normal
+#Ha: La distribucion no es normal
+
+lilliefors_albumin <- lillie.test(x = data$Albumin)
+print(lilliefors_albumin)
 
