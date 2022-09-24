5d797ae3ff6daa7a003fb9bc18bf708310678ab6
ariel-madrid <ariel.argomedo@usach.cl>
2022-09-24 12:18:20 -0300

    Se corrigen detalles

--- Laboratorio 1/Hepatitis-Data.R
+++ Laboratorio 1/Hepatitis-Data.R
@@ -8,10 +8,13 @@ library(naniar)
 library(corrplot)
 library(nortest)
 library(car)
-#Leer data set de Hepatitis.
+library(MASS)
+library(ROCR)
+
+# Leer data set de Hepatitis.
 data <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/hepatitis/hepatitis.data", fileEncoding = "UTF-8", sep = ",")
 
-#Asignar nombres mas representativos a las variables de acuerdo al archivo hepatitis.name.
+# Asignar nombres mas representativos a las variables de acuerdo al archivo hepatitis.name.
 names(data) <- c('Class', 'Age', 'Sex', 'Steroid', 'Antivirals', 'Fatigue', 'Malaise', 'Anorexia', 'Liver_Big', 'Liver_Firm', 'Spleen_Palpable',
                  'Spiders', 'Ascites', 'Varices', 'Bilirubin', 'Alk_Phosphate', 'Sgot', 'Albumin', 'Protime', 'Histology')
 
@@ -26,7 +29,7 @@ missing_values_per_patient <- data.frame(missing_values = missing_values_per_pat
                                 mutate(id = seq(nrow(data))) %>%
                                 arrange(desc(missing_values))
 
-#Obtener top 10 pacientes con más valores NA
+# Obtener top 10 pacientes con más valores NA
 ids_patient_top_ten_missing_Values <- missing_values_per_patient$id[1:10]
 
 # max_missing_values_patient <- which(missing_values_per_patient == max(missing_values_per_patient))
@@ -70,14 +73,14 @@ data$Sgot <- as.numeric(data$Sgot)
 data$Albumin <- as.numeric(data$Albumin)
 data$Histology <- as.factor(data$Histology)
 
-#Obtener la moda de una determinada columna
+# Obtener la moda de una determinada columna
 getmode <- function(v){
   v=v[nchar(as.character(v))>0]
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-#Impute Missing Data
+# Impute Missing Data
 for (cols in colnames(data)) {
   if (cols %in% names(data[,sapply(data, is.numeric)])) {
     data<-data%>%mutate(!!cols := replace(!!rlang::sym(cols), is.na(!!rlang::sym(cols)), mean(!!rlang::sym(cols), na.rm=TRUE)))
@@ -121,62 +124,62 @@ liver_b_plt <- summarise_aux(data, "Liver_Big") + scale_x_discrete("Higado Agran
 liver_f_plt <- summarise_aux(data, "Liver_Firm") + scale_x_discrete("Higado Firme")
 cathegorical_plots_pt_2 <- ggarrange(malaise_plt, anorex_plt, liver_b_plt, liver_f_plt, ncol = 2, nrow = 2)
 
-#Grafico de frecuencias para reconocer la moda de la variable Spleen Palpable
+# Grafico de frecuencias para reconocer la moda de la variable Spleen Palpable
 barplot_spleen_palpable <- ggplot(data,aes(x=factor(Spleen_Palpable)))+ geom_bar(position="dodge")+ labs(title="", x="Spleen Palpable", y = "Frecuencia")+
   geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
 
-#Grafico de frecuencias para reconocer la moda de la variable Spiders
+# Grafico de frecuencias para reconocer la moda de la variable Spiders
 barplot_spiders <- ggplot(data,aes(x=factor(Spiders)))+ geom_bar(position="dodge")+ labs(title="", x="Spiders", y = "Frecuencia")+
   geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
 
-#Grafico de frecuencias para reconocer la moda de la variable Ascites
+# Grafico de frecuencias para reconocer la moda de la variable Ascites
 barplot_ascites <- ggplot(data,aes(x=factor(Ascites)))+ geom_bar(position="dodge")+ labs(title="", x="Ascites", y = "Frecuencia")+
   geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
 
-#Grafico de frecuencias para reconocer la moda de la variable Varices
+# Grafico de frecuencias para reconocer la moda de la variable Varices
 barplot_varices <- ggplot(data,aes(x=factor(Varices)))+ geom_bar(position="dodge")+ labs(title="", x="Varices", y = "Frecuencia")+
   geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
 
-#Grafico de frecuencias para reconocer la moda de la variable Histology
+# Grafico de frecuencias para reconocer la moda de la variable Histology
 barplot_histology <- ggplot(data,aes(x=factor(Histology)))+ geom_bar(position="dodge")+ labs(title="", x="Histology", y = "Frecuencia")+
   geom_text(aes(label=..count..),stat='count',position=position_dodge(0.9),vjust=-0.2)
 
-#Combinar figuras
+# Combinar figuras
 final_barplot_combined <- ggarrange(barplot_spleen_palpable,barplot_spiders,barplot_ascites,barplot_varices,barplot_histology, nrow=2,ncol=4)
 print(final_barplot_combined)
 
-#Obtener matriz de correlacion para observar correlacion de las variables numéricas de la base de datos.
+# Obtener matriz de correlacion para observar correlacion de las variables numéricas de la base de datos.
 correlacion<-round(cor(select_if(data, is.numeric)), 1)
 
 corrplot(correlacion, method="number", type="upper")
 
-#Diagrama de dispersion que muestra leve relacion inversa entre Bilirubin y Albumin
+# Diagrama de dispersion que muestra leve relacion inversa entre Bilirubin y Albumin
 bilirubin_albumin_cor <- scatterplot(Bilirubin ~ Albumin,data = data, smooth = FALSE, grid = F, frame = F)
 print(bilirubin_albumin_cor)
 
-#Visualizar distribución de variables numéricas continuas
+# Visualizar distribución de variables numéricas continuas
 
-#Bilirubin distribution
+# Bilirubin distribution
 bilirubin_distribution <- ggplot(data, aes(x=Bilirubin)) + geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FFF666") + labs(title="", y = "Densidad") + 
   geom_vline(aes(xintercept=mean(Bilirubin, na.rm=T)), color="red", linetype="dashed", size=1)
 
-#Albumin distribution
+# Albumin distribution
 albumin_distribution <- ggplot(data, aes(x=Albumin)) + geom_histogram(aes(y=..density..), binwidth=.5, colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FFF666") + labs(title="", y = "Densidad") + 
   geom_vline(aes(xintercept=mean(Albumin, na.rm=T)), color="red", linetype="dashed", size=1)
 
-#Combinar gráficos
+# Combinar gráficos
 final_plot_distribution <- ggarrange(bilirubin_distribution,albumin_distribution, nrow=1,ncol=2)
 print(final_plot_distribution)
 
-#Gráfica de barras segmentadas para Class + Spleen_Palpable
+# Gráfica de barras segmentadas para Class + Spleen_Palpable
 levels(data$Class) <- c("Muerto","Vivo")
 levels(data$Spleen_Palpable) <- c("Yes", "No")
 contingency_table  <- xtabs(~ Class + Spleen_Palpable , data = data)
 contingency_table  <- as.data.frame(contingency_table)
 
-#Se crea el gráfico
+# Se crea el gráfico
 segmented_bar_plot <- ggplot(contingency_table, aes(fill = Class , y = Freq , x = Spleen_Palpable))
 segmented_bar_plot <- segmented_bar_plot + geom_bar(position = "stack", stat = "identity")
 segmented_bar_plot <- segmented_bar_plot + labs(y = "Frecuencia") + ggtitle("Barras  apiladas para Class y Spleen Palpable")
@@ -196,15 +199,17 @@ plot(age_hist)
 bilirrubin_class <- ggplot(data,aes(x = Class, y = Bilirubin, fill = Class))+
   geom_boxplot()
 
+print(bilirrubin_class)
+
 # Vemos que pacientes de clase 1 (no sobrevivientes) tuvieron cantidades 
 # más altas de bilirubina cuando padecieron la enfermedad.
 
 # Análisis inferencial ----
 
-#Test Lilliefors para determinar si variable Albumin sigue una distribución normal.
+# Test Lilliefors para determinar si variable Albumin sigue una distribución normal.
 
-#H0: La distribución es normal
-#Ha: La distribución no es normal
+# H0: La distribución es normal
+# Ha: La distribución no es normal
 
 lilliefors_albumin <- lillie.test(x = data$Albumin)
 print(lilliefors_albumin)
@@ -248,21 +253,21 @@ age_test <- wilcox.test(x = edad_1,
 
 cat("Test de Wilcoxon Mann Whitney para la edad, p-value = ", age_test$p.value)
 
-#Modelo de regresion logistica
+# Modelo de regresion logistica
 
-#Establecer semilla
-set.seed(666)
+# Establecer semilla
+set.seed(432)
 
-#Particionar datos
+# Particionar datos. Validacion cruzada
 sample <- sample(2,nrow(data),replace=T,prob = c(0.8,0.2))
 train <- data[sample==1,]
 test <- data[sample==2,]
 
-#Se crea el modelo completo
+# Se crea el modelo completo
 model <- glm(Class~., family = binomial(link = "logit"), data = train)
 print(summary(model))
 
-#Se seleccionan las variables mas significativas.
+# Se seleccionan las variables mas significativa en base al AIC.
 optimized_model <- model %>% stepAIC(trace = FALSE)
 print(summary(optimized_model))
 
@@ -271,7 +276,10 @@ probs_values_train <- predict(optimized_model, train, type = "response")
 probs_train <- ifelse(probs_values_train > 0.5, 1, 0)
 confusion_matrix_train <- table(Predicted = probs_train, Actual = train$Class)
 
-#Error de clasificacion
+cat("\n")
+print(confusion_matrix_train)
+
+#Error de clasificacion train data
 error_train <- 1 - sum(diag(confusion_matrix_train))/sum(confusion_matrix_train)
 cat("\nError de clasificacion train set: ",error_train,"\n")
 
@@ -280,15 +288,19 @@ probs_values_test <- predict(optimized_model, test, type = "response")
 probs_test <- ifelse(probs_values_test > 0.5, 1, 0)
 confusion_matrix_test <- table(Predicted = probs_test, Actual = test$Class)
 
-#Error de clasificacion
+cat("\n")
+print(confusion_matrix_test)
+
+# Error de clasificacion test data
 error_test <- 1 - sum(diag(confusion_matrix_test))/sum(confusion_matrix_test)
 cat("\nError de clasificacion test set: ",error_test,"\n")
 
-#Estudiar poder predictivo del modelo.
+# Estudiar poder predictivo del modelo.
 prediction <- prediction(probs_values_test, test$Class)
 performance <- performance(prediction, measure = "tpr", x.measure = "fpr")
 plot(performance)
 
+# Mostrar valor de AUC
 auc <- performance(prediction, measure = "auc")
 auc <- auc@y.values[[1]]
 cat("\n AUC: ",auc,"\n")
